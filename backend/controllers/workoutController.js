f33eea9380136ce3bd149cd8eb5e6adce7971cdf
robpwatkins <robpwatkins@gmail.com>
2023-02-19 21:35:40 -0600

    authhhh

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const { isValid } = mongoose.Types.ObjectId;
 
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  const user_id = req.user._id;
+  const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 });
   res.status(200).json(workouts);
 };
 
@@ -29,7 +30,8 @@ const createWorkout = async (req, res) => {
   }
 
   try {
-    const workout = await Workout.create({ title, reps, load });
+    const user_id = req.user._id;
+    const workout = await Workout.create({ title, reps, load, user_id });
     res.status(200).json(workout);
   } catch (error) {
     res.status(400).json({ error: error.message });
