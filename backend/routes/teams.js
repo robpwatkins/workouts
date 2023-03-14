8798afd7b048f1e3bdf98c2abb8a59279991e010
robpwatkins <robpwatkins@gmail.com>
2023-03-13 21:22:18 -0500

    adding teams.json with team colors

--- backend/routes/teams.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getTeams, createTeam } = require('../controllers/teamController');
-
-const router = express.Router();
-
-router.get('/', getTeams);
-router.post('/', createTeam);
-
-module.exports = router;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,56 +1,36 @@
-import { useEffect, useState } from 'react';
-// import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
-// import { useAuthContext } from '../hooks/useAuthContext';
+import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import { useAuthContext } from '../hooks/useAuthContext';
 
-// import WorkoutDetails from '../components/WorkoutDetails';
-// import WorkoutForm from '../components/WorkoutForm';
-import TeamDetails from '../components/TeamDetails';
-import TeamForm from '../components/TeamsForm';
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-  // const { workouts, dispatch } = useWorkoutsContext();
-  // const { user } = useAuthContext();
-  const [teams, setTeams] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchWorkouts = async () => {
-  //     const response = await fetch('/api/workouts', {
-  //       headers: { 'Authorization': `Bearer ${user.token}` }
-  //     });
-  //     const json = await response.json();
-
-  //     if (response.ok) dispatch({ type: 'SET_WORKOUTS', payload: json });
-  //   }
-
-  //   if (user) fetchWorkouts();
-  //   if (user) console.log('Home user: ', user);
-  // }, [dispatch, user]);
+  const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
-    const fetchTeams = async () => {
-      const response = await fetch('/api/teams');
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts', {
+        headers: { 'Authorization': `Bearer ${user.token}` }
+      });
       const json = await response.json();
 
-      if (response.ok) setTeams(json);
+      if (response.ok) dispatch({ type: 'SET_WORKOUTS', payload: json });
     }
 
-    fetchTeams();
-  }, [teams]);
+    if (user) fetchWorkouts();
+    // if (user) console.log('Home user: ', user);
+  }, [dispatch, user]);
 
   return (
     <div className="home">
-      {/* <div className="workouts">
+      <div className="workouts">
         {workouts && workouts.map(workout => (
           <WorkoutDetails key={workout._id} workout={workout} />
         ))}
       </div>
-      <WorkoutForm /> */}
-      <div className="teams">
-        {teams && teams.map(team => (
-          <TeamDetails key={team._id} team={team} />
-        ))}
-      </div>
-      <TeamForm />
+      <WorkoutForm />
     </div>
   )
 }
