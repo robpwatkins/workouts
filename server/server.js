d8b84a0e9fed551428bb1d8b1817927164712bdc
robpwatkins <robpwatkins@gmail.com>
2024-03-09 17:08:37 -0600

    loggin user at /user

--- server/server.js
+++ server/server.js
@@ -46,6 +46,7 @@ app.use('/api/user', userRoutes);
 app.use('/auth', authRoutes);
 
 app.get('/user', (req, res) => {
+  console.log('req.user: ', req.user);
   try {
     if (req.session.messages && req.session.messages.length) {
       const [error] = req.session.messages;
