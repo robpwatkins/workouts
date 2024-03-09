302c8dfb5eb1ff03eb8ab41f9e72115a85ad9948
robpwatkins <robpwatkins@gmail.com>
2024-03-09 13:06:09 -0600

    updating login server url

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
   };
   
   const handleClick = (buttonType) => {
-    window.open(`http://localhost:4001/auth/${buttonType}`, '_self');
+    window.open(`${process.env.REACT_APP_SERVER_URL}/auth/${buttonType}`, '_self');
     // window.open(`/auth/${buttonType}`, '_self');
   }
 
