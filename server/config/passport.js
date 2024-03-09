38bfdd182628bad91f71f37ee44aad0dbc2461c1
robpwatkins <robpwatkins@gmail.com>
2024-03-09 16:56:18 -0600

    logging user

--- server/config/passport.js
+++ server/config/passport.js
@@ -49,6 +49,7 @@ module.exports = function(passport) {
 
     try {
       let user = await User.findOne({ googleId: profile.id });
+      console.log('user: ', user);
 
       if (user) done(null, user);
       else {
