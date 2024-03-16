9806fac8c63d9cf1ae1dbc01cab464f2136b2732
robpwatkins <robpwatkins@gmail.com>
2024-03-16 12:58:52 -0500

    adding production url to Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
   };
   
   const handleClick = (buttonType) => {
-    window.open(`http://localhost:4001/auth/${buttonType}`, '_self');
+    window.open(`https://mlb-fantasy.fly.dev/auth/${buttonType}`, '_self');
     // window.open(`/auth/${buttonType}`, '_self');
   }
 
