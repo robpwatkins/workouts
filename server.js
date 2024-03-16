81bd49421b8851460e4212df611455629cacb4e9
robpwatkins <robpwatkins@gmail.com>
2024-03-16 08:38:19 -0500

    updating default port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require('./config/passport')(passport);
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
