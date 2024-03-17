dd4c3fa02373d839e16b6dd102f2130b95546dd6
robpwatkins <robpwatkins@gmail.com>
2024-03-16 19:30:57 -0500

    adding bangs

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/all-series', async (req, res) => {
   res.json(allSeries);
 });
 
-app.get('/ping', (req, res) => res.send('pong!'));
+app.get('/ping', (req, res) => res.send('pong!!!'));
 
 // app.use(express.static(path.join(__dirname, "./client/build")));
 // app.get("*", (_, res) => {
