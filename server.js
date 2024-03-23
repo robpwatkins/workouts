ec4c5dd78053ed3bb18b690d360a4b27eb7faf3b
robpwatkins <robpwatkins@gmail.com>
2024-03-23 09:49:15 -0500

    updating cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://api.theserieschallenge.com'],
+  origin: ['http://localhost:3000', 'https://theserieschallenge.com'],
   credentials: true
 }));
 
