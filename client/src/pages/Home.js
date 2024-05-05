d38dc54b427d0605d36ed6bc3f45b6e7400eba89
robpwatkins <53236017+robpwatkins@users.noreply.github.com>
2024-05-05 10:12:31 -0600

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
     const fetchAllSeries = async () => {
       const response = await fetch(`${serverUrl}/all-series`);
       const json = await response.json();
-      setAllSeries(json.slice(0, 18));
+      setAllSeries(json.slice(0, 17));
     };
 
     const fetchPicks = async () => {
